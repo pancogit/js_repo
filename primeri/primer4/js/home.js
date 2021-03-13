68e433f6f77070309cdf5b90d30556344145b568
Panco <panco@pancic.com>
2021-03-13 21:23:59 +0100

    Gotov javascript za kategorije i recepte za primer 4.

--- primeri/primer4/js/home.js
+++ primeri/primer4/js/home.js
@@ -1,18 +1,26 @@
 // homepage
 
 
+// import classes from javascript modules
 import { Fetch } from './fetch.js';
+import { Categories } from './categories.js';
+import { Recipes } from './recipes.js';
 
 var fetch = new Fetch();
 
 fetch.getDataFromServer()
         .then(dataFetched.bind(null, fetch))  // send fetch object via bind
-        .catch(dataNotFetched);
+        .catch(errorHandler);
 
+// add remaining javascript when data is fetched from server
 function dataFetched(value) {
+    var categoriesData = value.data.recipes.categories;
+    var recipes = new Recipes(categoriesData);
+    var categories = new Categories(categoriesData, recipes);
 
+    categories.addCategories();
 }
 
-function dataNotFetched(reason) {
-
+function errorHandler(reason) {
+    console.error(reason.stack);
 }
